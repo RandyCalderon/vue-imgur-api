18ab5075d52712051229e3ce6913bfd1c75c8c04
Randy Calderon <randycweb@gmail.com>
2019-03-27 23:00:38 -0700

    Added logout functionality

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -1,7 +1,8 @@
 import api from '../../api/imgur';
+import qs from 'qs';
 
 const state = {
-  token: null,
+  token: window.localStorage.getItem('imgur_token'),
 };
 
 const getters = {
@@ -12,9 +13,15 @@ const actions = {
   login: () => {
     api.login();
   },
+  finalizeLogin({ commit }, hash) {
+    const query = qs.parse(hash.replace('#', ''));
+    commit('setToken', query.access_token);
+    window.localStorage.setItem('imgur_token', query.access_token);
+  },
   // To call mutation call commit
   logout: ({ commit }) => {
     commit('setToken', null);
+    window.localStorage.removeItem('imgur_token');
   },
 };
 
